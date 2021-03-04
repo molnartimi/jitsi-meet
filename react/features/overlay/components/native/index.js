8e796f9765d499079e8a34a328393328cb7a62de
Molnár Tímea <molnart@sonrisa.hu>
2021-03-04 10:58:16 +0100

    Revert "VS-2792 Remove Jitsi default error overlay, send connection error back to native app. (#75)"
    This reverts commit 97109ef9231a932b0597bb86c79eec2f322cd245.

--- react/features/overlay/components/native/index.js
+++ react/features/overlay/components/native/index.js
@@ -2,3 +2,4 @@
 
 export { default as LoadConfigOverlay } from './LoadConfigOverlay';
 export { default as OverlayFrame } from './OverlayFrame';
+export { default as PageReloadOverlay } from './PageReloadOverlay';
