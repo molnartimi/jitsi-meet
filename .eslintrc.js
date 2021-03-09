2572f0940c00b44a5da262d519be69b616ae9663
Barna Bruder <bruder.barna@sonrisa.hu>
2021-03-09 10:58:56 +0100

    remove no-invalid-this eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,8 @@ module.exports = {
     ],
     rules: {
         'require-jsdoc': 'off',
-        'no-confusing-arrow': 'off'
+        'no-confusing-arrow': 'off',
+        'no-invalid-this': 'off'
     },
     settings: {
         react: {
